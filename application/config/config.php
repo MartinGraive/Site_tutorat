2c0af171896458fc6b249ec0284ac8e40a5f256f
Biohazard50 <reouven@reouven-PC.(none)>
2013-04-15 12:49:14 +0200

    Changement de nom dans les fichiers de configuration

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/tueurs-chaptal/';
+$config['base_url']	= 'http://localhost/Site_tutorat/';
 
 /*
 |--------------------------------------------------------------------------
